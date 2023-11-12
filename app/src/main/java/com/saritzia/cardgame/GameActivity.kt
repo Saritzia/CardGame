cc3d0560c97a0db16b83d50f2899315b1accf2fa
Saritzia <saralopez0988@gmail.com>
2023-11-12 15:30:24 +0100

    Game activity finished, buttons do not work properly

--- app/src/main/java/com/saritzia/cardgame/GameActivity.kt
+++ app/src/main/java/com/saritzia/cardgame/GameActivity.kt
@@ -1,14 +1,77 @@
 package com.saritzia.cardgame
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.saritzia.cardgame.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.saritzia.cardgame.databinding.ActivityGameBinding
+import kotlin.random.Random
 
 class GameActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+    private val cards = arrayOf(R.drawable.c1,R.drawable.c2,R.drawable.c3,R.drawable.c4,R.drawable.c5,R.drawable.c6,
+        R.drawable.c7,R.drawable.c8,R.drawable.c9,R.drawable.c10,R.drawable.c11,R.drawable.c12,R.drawable.c13)
+    private var score: Int = 0
+    private var previousPosition: Int = getNewPosition()
+    private var currentPosition: Int = previousPosition
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.userName.text = intent.extras?.getString("userName") ?: "User"
+        binding.scoreText.text = score.toString()
+        getNewCard(previousPosition)
+        binding.upButton.setOnClickListener {
+            buttonTapped()
+        }
+        binding.downButton.setOnClickListener {
+            buttonTapped()
+        }
     }
 
+    private fun getNewPosition(): Int {
+       return Random.nextInt(0,cards.size)
+    }
+
+    private fun getNewCard(position: Int) {
+        binding.card.setImageResource(cards[position])
+    }
+
+    private fun checkCurrentCardIsGreater(): Boolean {
+        return currentPosition > previousPosition
+    }
+
+    private fun buttonTapped() {
+        previousPosition = currentPosition
+        currentPosition = getNewPosition()
+        if(currentPosition == previousPosition) {
+            Snackbar.make(binding.root,getString(R.string.sameCardText),Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        when{
+            binding.upButton.callOnClick() -> {
+                if (checkCurrentCardIsGreater()) {
+                    score ++
+                }else{
+                    createGameOverSnackBar()
+                }
+            }
+            binding.downButton.callOnClick() ->
+                if (!checkCurrentCardIsGreater()) {
+                    score ++
+                }else{
+                    createGameOverSnackBar()
+                }
+            }
+        }
+
+    private fun createGameOverSnackBar() {
+        val snackbar = Snackbar.make(binding.root,"Game over, puntuación: $score",Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction(getString(R.string.backLiteral)){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        snackbar.show()
+    }
 }
--- app/src/main/java/com/saritzia/cardgame/MainActivity.kt
+++ app/src/main/java/com/saritzia/cardgame/MainActivity.kt
@@ -27,21 +27,22 @@ class MainActivity : AppCompatActivity() {
     private fun createSnackBar(userName: Editable) {
         val snackbar: Snackbar
         if (userName.isNotEmpty()) {
-            snackbar = make(binding.root, "@userName.toSring()@string/startMessage",
+            val message = "$userName ${getString(R.string.startMessage)}"
+            snackbar = make(binding.root, message,
                 LENGTH_INDEFINITE
             )
-            snackbar.setAction("@string/okLiteral",View.OnClickListener {
+            snackbar.setAction(getString(R.string.okLiteral)) {
                 navigateToNextActivity(userName.toString())
-            })
+            }
             snackbar.show()
         }else {
-            snackbar = make(binding.root,"@string/emptyUserSnackBar", LENGTH_SHORT)
+            snackbar = make(binding.root,getString(R.string.emptyUserSnackBar), LENGTH_SHORT)
             snackbar.show()
         }
     }
 
     private fun navigateToNextActivity(userName: String) {
-        val intent = Intent(applicationContext, GameActivity::class.java)
+        val intent = Intent(this, GameActivity::class.java)
         intent.putExtra("userName", userName)
         startActivity(intent)
     }
